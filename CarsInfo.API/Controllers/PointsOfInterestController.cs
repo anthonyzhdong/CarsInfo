ff77a2a424b148f2be930a85246d5f17112cc8d1
Anthony <anthonyzhdong@gmail.com>
2025-05-13 10:40:23 +1200

    create & update

--- CarsInfo.API/Controllers/PointsOfInterestController.cs
+++ CarsInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CarsInfo.API.Models;
+using CarsInfo.Api.Models;
 
 namespace CarsInfo.API.Controllers
 {
@@ -18,7 +19,7 @@ namespace CarsInfo.API.Controllers
             return Ok(car.PointsOfInterest);
         }
 
-        [HttpGet("{pointOfInterestId}")]
+        [HttpGet("{pointOfInterestId}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int carId, int pointOfInterestId)
         {
             var car = CarsDataStore.Current.Cars.FirstOrDefault(c => c.Id == carId);
@@ -33,6 +34,54 @@ namespace CarsInfo.API.Controllers
             }
             return Ok(pointOfInterest);
         }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(int carId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+
+            var car = CarsDataStore.Current.Cars.FirstOrDefault(c => c.Id == carId);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            var maxPointOfInterestId = CarsDataStore.Current.Cars.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var newPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            car.PointsOfInterest.Add(newPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { carId = carId, pointOfInterestId = newPointOfInterest.Id }, newPointOfInterest);
+
+
+        }
+        // id of point of interest
+        [HttpPut("{pointOfInterestId}")]
+        public ActionResult UpdatePointOfInterest(int carId, int pointOfInterestId, PointoFInterestForUpdateDto pointOfInterest)
+        {
+            var car = CarsDataStore.Current.Cars.FirstOrDefault(c => c.Id == carId);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = car.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+            return NoContent();
+
+        }
+
+
 
     }
 }
---
+++ CarsInfo.API/Models/PointoFInterestForUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CarsInfo.Api.Models
+{
+    public class PointoFInterestForUpdateDto
+    {
+        [Required(ErrorMessage = "You should provide a name value")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(10)]
+        public string? Description { get; set; };
+    }
+}
