9fd6db79d1439ea6bc746dafc5c58c47a583abd5
Anthony <anthonyzhdong@gmail.com>
2025-05-12 16:35:50 +1200

    file

---
+++ CarsInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace CarsInfo.Api.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+
+            var pathToFile = "porsche.jpg";
+
+            if (!System.IO.File.Exists(pathToFile))
+
+            {
+                return NotFound();
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, "image/jpeg", Path.GetFileName(pathToFile));
+        }
+    }
+}
--- CarsInfo.API/Controllers/PointsOfInterestController.cs
+++ CarsInfo.API/Controllers/PointsOfInterestController.cs
@@ -18,6 +18,21 @@ namespace CarsInfo.API.Controllers
             return Ok(car.PointsOfInterest);
         }
 
+        [HttpGet("{pointOfInterestId}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int carId, int pointOfInterestId)
+        {
+            var car = CarsDataStore.Current.Cars.FirstOrDefault(c => c.Id == carId);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = car.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
 
     }
 }
